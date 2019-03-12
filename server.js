2683074619923156651bceecffc9c9c7b25d074a
Adam Phillips-Silver <adam.phillips-silver@handshake.com>
2019-03-11 23:56:47 -0400

    basic websocket timer implemented

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express"),
     WebSocket = require("ws"),
     http = require("http"),
     path = require("path"),
+    os = require('os'),
     SERVER_PORT = 3000,
     DIST_DIR = __dirname,
     HTML_FILE = path.join(DIST_DIR, "index.html");
@@ -14,9 +15,21 @@ const websocketServer = new WebSocket.Server({ server });
 
 app.use(express.static(DIST_DIR));
 
-websocketServer.on("connection", (webSocketClient) =>
-    webSocketClient.send("Websocket Connected!")
-);
+websocketServer.on("connection", (wsc) => {
+    wsc.send("Websocket Connected!");
+    setInterval(
+        () => {
+            // in the land of multi-cpu computers the avg needs to be normalized by number of CPUs 
+            const cpus = os.cpus().length;
+            const avg = os.loadavg()[0]/cpus 
+            wsc.send(`{
+                time: ${Date.now()},
+                value: ${avg}
+            }`);
+        },
+        10000
+    );
+});
 
 app.get("/", (_req, res) => res.sendFile(HTML_FILE));
 
